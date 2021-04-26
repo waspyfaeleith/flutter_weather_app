27b9924b6c7df08d52cd2a0f24112148af8afb9b
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2021-04-26 14:51:44 +0100

    lesson 147 completed - networking with the HTTP package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:clima_flutter_latest/services/location.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -12,6 +13,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -19,7 +21,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     print(location.latitude);
     print(location.longitude);
+  }
 
+  void getData() async {
+    Uri url = Uri.parse('https://samples.openweathermap.org/agro/1.0/weather?lat=35&lon=139&appid=b1b15e88fa797225412429c1c50c122a1');
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
