a776616de6a0733cb69de19c447e7a9c39a5b55c
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2021-04-26 14:23:55 +0100

    initial commit - just before lesson 146

---
+++ android/app/src/main/kotlin/com/example/clima_flutter_latest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clima_flutter_latest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,29 @@
+import 'package:clima_flutter_latest/services/location.dart';
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
