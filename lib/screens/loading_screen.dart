2dafec9309e12940a6d8499d43aa3da06f4c2387
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2021-04-26 15:44:42 +0100

    lesson 148 completed - getting live data from weather API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,7 @@
 import 'package:clima_flutter_latest/services/location.dart';
-import 'package:http/http.dart' as http;
+import 'package:clima_flutter_latest/services/networking.dart';
+import 'package:clima_flutter_latest/utilities/apiKey.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,50 +10,27 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
-    getData();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(url: 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+
   }
 
-  void getData() async {
-
-    //Uri url = Uri.parse('https://samples.openweathermap.org/agro/1.0/weather?lat=35&lon=139&appid=b1b15e88fa797225412429c1c50c122a1');
-    Uri url = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1');
-
-    http.Response response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      String data = response.body;
-      //print(data);
-
-     // var longitude = jsonDecode(data)['coord']['lon'];
-     // print(longitude);
-     //
-     // var weatherDescription = jsonDecode(data)['weather'][0]['description'];
-     // print(weatherDescription);
-      var decodeData = jsonDecode(data);
-
-      double temperature = decodeData['main']['temp'];
-      int condition = decodeData['weather'][0]['id'];
-      String cityName = decodeData['name'];
-
-      print(temperature);
-      print(condition);
-      print(cityName);
-
-    } else {
-      print(response.statusCode);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
