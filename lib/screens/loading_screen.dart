cb29698a18169f520a34c1652b8e69ca55231bc2
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2021-04-26 16:46:50 +0100

    lesson 152 completed - refactoring location methods

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,4 @@
-import 'package:clima_flutter_latest/services/location.dart';
-import 'package:clima_flutter_latest/services/networking.dart';
-import 'package:clima_flutter_latest/utilities/apiKey.dart';
+import 'package:clima_flutter_latest/services/weather.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -20,12 +18,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
 
-    NetworkHelper networkHelper = NetworkHelper(url: 'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-
-    var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute( builder: (context) {
       return LocationScreen(locationWeather: weatherData,);
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,23 @@
+import 'package:clima_flutter_latest/services/location.dart';
+import 'package:clima_flutter_latest/services/networking.dart';
+import 'package:clima_flutter_latest/utilities/apiKey.dart';
+
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(url: '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
