c4bc7d21dd9f6b2e0e64c3f49f036e68dee7fc03
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2021-04-26 15:14:18 +0100

    lesson 147 completed - JSON Parsing and Dynamic Types

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clima_flutter_latest/services/location.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -24,12 +25,31 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    Uri url = Uri.parse('https://samples.openweathermap.org/agro/1.0/weather?lat=35&lon=139&appid=b1b15e88fa797225412429c1c50c122a1');
+
+    //Uri url = Uri.parse('https://samples.openweathermap.org/agro/1.0/weather?lat=35&lon=139&appid=b1b15e88fa797225412429c1c50c122a1');
+    Uri url = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1');
+
     http.Response response = await http.get(url);
 
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+      //print(data);
+
+     // var longitude = jsonDecode(data)['coord']['lon'];
+     // print(longitude);
+     //
+     // var weatherDescription = jsonDecode(data)['weather'][0]['description'];
+     // print(weatherDescription);
+      var decodeData = jsonDecode(data);
+
+      double temperature = decodeData['main']['temp'];
+      int condition = decodeData['weather'][0]['id'];
+      String cityName = decodeData['name'];
+
+      print(temperature);
+      print(condition);
+      print(cityName);
+
     } else {
       print(response.statusCode);
     }
